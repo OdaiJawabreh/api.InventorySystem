d07d83c2dd891e92a9151bc42249c0d333652196
OdaiJawabreh <odaijawabreh.oj@gmail.com>
2023-12-31 19:15:46 +0300

    Use .Env File

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
       port: 3306,
-      username: 'root',
-      password: '0000',
-      database: 'InventorySystem',
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
       entities: [],
       synchronize: true,
     }),
