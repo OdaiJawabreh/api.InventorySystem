fa99828cdd0dfeb5a289e810bd66c64a29931d70
OdaiJawabreh <odaijawabreh.oj@gmail.com>
2023-12-31 20:07:58 +0300

    initiate transaction module

---
+++ src/transaction/entities/transaction.entity.ts
@@ -0,0 +1,23 @@
+// transaction.entity.ts
+
+import { Entity, PrimaryGeneratedColumn, Column, ManyToMany, ManyToOne } from 'typeorm';
+import { Product } from '../../product/entities/product.entity';
+import { User } from '../../user/entities/user.entity';
+
+@Entity()
+export class Transaction {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToMany(() => Product)
+  products: Product[];
+
+  @Column('decimal', { precision: 10, scale: 2 })
+  totalAmount: number;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  timestamp: Date;
+
+  @ManyToOne(() => User, user => user.transactions)
+  user: User;
+}
