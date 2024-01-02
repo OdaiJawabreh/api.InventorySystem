e292dbb5605834bbb2315741cbb19c500da4da75
OdaiJawabreh <odaijawabreh.oj@gmail.com>
2024-01-02 17:32:11 +0300

    transaction.dto

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,13 @@
-export class CreateTransactionDto {}
+// transaction.dto.ts
+import { IsArray, IsDecimal, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsArray()
+  @IsNotEmpty()
+  products: number[]; // An array of product IDs
+
+  @IsDecimal({ decimal_digits: '2', force_decimal: true })
+  @IsNotEmpty()
+  @IsNumber()
+  totalAmount: number;
+}
