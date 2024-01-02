240feee3f16ae36326e9df49c4f58374c24a3236
OdaiJawabreh <odaijawabreh.oj@gmail.com>
2024-01-02 20:26:47 +0300

    Get Transaction By User Id

--- src/transaction/transaction.service.ts
+++ src/transaction/transaction.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -13,38 +13,56 @@ export class TransactionService {
     private readonly transactionRepository: Repository<Transaction>,
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
-
   ) {}
 
-  async createTransaction(createTransactionDto: CreateTransactionDto): Promise<Transaction> {
-    // Extract data from the DTO
-    const { products, userId, totalAmount } = createTransactionDto;
-  
-    const newTransaction = new Transaction();
-    newTransaction.totalAmount = totalAmount;
-    newTransaction.user = { id: userId } as User;
-    newTransaction.products = products;
-    const savedTransaction = await this.transactionRepository.save(newTransaction);
-    // now i want to update the stock
-    for (let i = 0; i < products.length; i++) {
-      const productEntity = products[i]
-      productEntity.stockQuantity -= 1; 
-      await this.productRepository.save(productEntity);
-    }
+  async createTransaction(
+    createTransactionDto: CreateTransactionDto,
+  ): Promise<Transaction> {
+    try {
+      // Extract data from the DTO
+      const { products, userId, totalAmount } = createTransactionDto;
 
-  
-    return savedTransaction;
+      const newTransaction = new Transaction();
+      newTransaction.totalAmount = totalAmount;
+      newTransaction.user = { id: userId } as User;
+      newTransaction.products = products;
+      const savedTransaction =
+        await this.transactionRepository.save(newTransaction);
+      // now i want to update the stock
+      for (let i = 0; i < products.length; i++) {
+        const productEntity = products[i];
+        productEntity.stockQuantity -= 1;
+        await this.productRepository.save(productEntity);
+      }
+
+      return savedTransaction;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Error in createTransaction:',
+        error,
+      );
+    }
   }
-  
-  findAll() {
-    return `This action returns all transaction`;
+
+  async findAll(userId: any): Promise<Transaction[]> {
+    try {
+      const transactions = await this.transactionRepository.find({
+        where: { user: { id: userId } },
+        relations: ['products'],
+      });
+
+      return transactions;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Error in createTransaction:',
+        error,
+      );
+    }
   }
 
   findOne(id: number) {
     return `This action returns a #${id} transaction`;
   }
-
-
 
   remove(id: number) {
     return `This action removes a #${id} transaction`;
