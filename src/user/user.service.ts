40597ca3eb9f027761da3df063ae72528f1f2390
OdaiJawabreh <odaijawabreh.oj@gmail.com>
2024-01-02 21:26:16 +0300

    Add Two user By defalt

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,10 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, OnModuleInit } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { UserRole } from 'src/shared/enum';
 
 @Injectable()
 export class UserService {
@@ -11,6 +12,41 @@ export class UserService {
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
+
+  async onModuleInit() {
+    // Check if an admin user with the specified email and name exists
+    const adminUserExists = await this.usersRepository.findOne({
+      where: { role: UserRole.Admin },
+    });
+    const meberUserExists = await this.usersRepository.findOne({
+      where: { role: UserRole.Member },
+    });
+
+    // If no admin user exists, create one
+    if (!adminUserExists) {
+      const adminUserDto = {
+        email: 'admin@nard.go',
+        name: 'admin',
+        role: UserRole.Admin,
+        password: '1234567',
+      };
+
+      // Create the admin user
+      await this.create(adminUserDto);
+    }
+    // If no mebeber user exists, create one
+    if (!meberUserExists) {
+      const memberUserDto = {
+        email: 'member@nard.go',
+        name: 'member',
+        role: UserRole.Member,
+        password: '1234567',
+      };
+
+      // Create the admin user
+      await this.create(memberUserDto);
+    }
+  }
 
   async create(createUserDto: CreateUserDto): Promise<User> {
     const { password, email } = createUserDto;
